123209218861a3ec5be2812888a4bee864836d5a
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-02-25 13:25:03 +0530

    Update diameterBinaryTree.py

--- Another 2/diameterBinaryTree.py
+++ Another 2/diameterBinaryTree.py
@@ -19,11 +19,50 @@ def diameter(root):
     option3 = diameter(root.right)
     return max(option1, max(option2, option3))
 
+# Complexity can be improved by getting diameter and heigth of left and right together
+# O(n) because every node does constant time
+# Or by storing the heigths in an array
 
+def height1(root, ans):
+    if (root == None):
+        return 0
+    leftHeight = height1(root.left, ans)
+
+    rightHeight = height1(root.right, ans)
+    ans[0] = max(ans[0], 1 + leftHeight + rightHeight)
+
+    return 1 + max(leftHeight, rightHeight)
+
+def diameter1(root):
+    if (root == None):
+        return 0
+    ans = []
+    heightOfTree = height1(root, ans)
+    return ans[0]
+
+def diameter2(root):
+    if root == None:
+        output = [0,0]
+        output[0] = 0
+        output[1] = 0
+        return output
+    lo = diameter2(root.left)
+    ro = diameter2(root.right)
+    height = 1 + max(lo[0], ro[0])
+    option1 = lo[0] + ro[0]
+    option2 = lo[1]
+    option3 = ro[1]
+    diameter = max(option1, max(option2, option3))
+    output = [0,0]
+    output[0] = height
+    output[1] = diameter
+    return output
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-print ("Diameter of given binary tree is %d" % (diameter(root)))
+#print ("Diameter of given binary tree is %d" % (diameter(root)))
+#print("Diameter of given binary tree is %d" % (diameter1(root)))
+print("Diameter of given binary tree is %d" % (diameter2(root)[1]))
