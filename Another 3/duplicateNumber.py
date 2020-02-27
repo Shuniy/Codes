d8a5af9d21eeace4d990611f9f9d45cbc463ddb6
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-02-27 12:19:36 +0530

    Update duplicateNumber.py

--- Another 3/duplicateNumber.py
+++ Another 3/duplicateNumber.py
@@ -16,3 +16,36 @@ def duplicateNumber(arr):
             return i
         else:
             pass
+
+
+def test_function(test_case):
+    arr = test_case[0]
+    solution = test_case[1]
+    output = duplicateNumber(arr)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+arr = [0, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+arr = [0, 2, 3, 1, 4, 5, 3]
+solution = 3
+
+test_case = [arr, solution]
+test_function(test_case)
+arr = [0, 1, 5, 4, 3, 2, 0]
+solution = 0
+
+test_case = [arr, solution]
+test_function(test_case)
+
+arr = [0, 1, 5, 5, 3, 2, 4]
+solution = 5
+
+test_case = [arr, solution]
+test_function(test_case)
