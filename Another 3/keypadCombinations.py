6994043b0f9408245885e57f0a7a8a4c60b35cd4
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-03-02 17:50:32 +0530

    Create keypadCombinations.py

---
+++ Another 3/keypadCombinations.py
@@ -0,0 +1,64 @@
+def get_characters(num):
+    if num == 2:
+        return "abc"
+    elif num == 3:
+        return "def"
+    elif num == 4:
+        return "ghi"
+    elif num == 5:
+        return "jkl"
+    elif num == 6:
+        return "mno"
+    elif num == 7:
+        return "pqrs"
+    elif num == 8:
+        return "tuv"
+    elif num == 9:
+        return "wxyz"
+    else:
+        return ""
+
+def keypad(num):
+    if num == 0:
+        return [""]
+    elif num / 10 < 1:
+        return [element for element in get_characters(num)]
+    else:
+        input_level = get_characters(num % 10)
+        input = keypad(num // 10)
+        result = []
+        for i in input_level:
+            for j in input:
+                result.append(j + i)
+        return result
+
+
+
+def test_keypad(input, expected_output):
+    if sorted(keypad(input)) == expected_output:
+        print("Yay. We got it right.")
+    else:
+        print("Oops! That was incorrect.")
+
+
+input = 0
+expected_output = [""]
+test_keypad(input, expected_output)
+
+input = 23
+expected_output = sorted(
+    ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+test_keypad(input, expected_output)
+
+input = 32
+expected_output = sorted(
+    ["da", "db", "dc", "ea", "eb", "ec", "fa", "fb", "fc"])
+test_keypad(input, expected_output)
+
+input = 8
+expected_output = sorted(["t", "u", "v"])
+test_keypad(input, expected_output)
+
+expected_output = sorted(["djg", "ejg", "fjg", "dkg", "ekg", "fkg", "dlg", "elg", "flg", "djh", "ejh", "fjh",
+                          "dkh", "ekh", "fkh", "dlh", "elh", "flh", "dji", "eji", "fji", "dki", "eki", "fki", "dli", "eli", "fli"])
+test_keypad(input, expected_output)
