90b15c3c99683d146f8fb5b21b7be343a5cdac6a
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-03-16 18:32:22 +0530

    Create ActiveDirectory.py

---
+++ Another 2/P1/ActiveDirectory.py
@@ -0,0 +1,6 @@
+"""
+Active Directory
+In Windows Active Directory, a group can consist of user(s) and group(s) themselves. We can construct this hierarchy as such. Where User is represented by str representing their ids.
+
+Write a function that provides an efficient look up of whether the user is in a group.
+"""
