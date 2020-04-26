fea6d2bbee093e7373ab0bd08e09a40ab049e5c8
Shubham Kumar <51149063+ZippySphinx@users.noreply.github.com>
2020-04-26 20:21:50 +0530

    Create largest_sum_non_adjacent_numbers.py

---
+++ Another 4/largest_sum_non_adjacent_numbers.py
@@ -0,0 +1,14 @@
+import sys
+
+def get_largest_non_adjacent_sum(array):
+    previous, largest = 0, 0
+    for amount in array:
+        print("amount: {}; previous: {}; largest: {}".format(
+            amount, previous, largest))
+        previous, largest = largest, max(largest, previous + amount)
+        print("new_previous: {}; new_largest: {}".format(previous, largest))
+    return largest
+
+
+print(get_largest_non_adjacent_sum([2, 4, 6, 8]))
+print(get_largest_non_adjacent_sum([5, 1, 1, 5]))
